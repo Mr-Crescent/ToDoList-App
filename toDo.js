b89cfb2117c2d3a16c088054648204262d74d3e3
Meshak <meshcrescent@gmail.com>
2025-10-25 13:10:05 -0400

    Added the funstionality of the To-Do- App

---
+++ toDo.js
@@ -0,0 +1,34 @@
+const mainBody = document.querySelector(".main-container")
+const clickBtn = document.querySelector(".btn")
+const inputValue = document.querySelector(".input")
+
+clickBtn.addEventListener("click", ()=> {
+    if(inputValue.value.trim() === "") {
+        alert("Please enter a task before adding it")
+        return;
+    } else {
+        const cancelImg = document.createElement("img")
+        cancelImg.src = "./images/cancelBtn.png"
+
+        const divContainer = document.createElement("div")
+        mainBody.append(divContainer)
+
+        const uncheckedImg = document.createElement("img")
+        uncheckedImg.src = "./images/unchecked-50.png"
+        const elementCreated = document.createElement("h2")
+        elementCreated.textContent = inputValue.value 
+        divContainer.append(uncheckedImg, elementCreated, cancelImg)
+
+        uncheckedImg.style.height = "2.2vh"
+        uncheckedImg.style.marginTop = "0.3em"
+        uncheckedImg.style.marginRight = "1.5em"
+        elementCreated.style.fontWeight = "100"
+        elementCreated.style.fontFamily = "Caveat" 
+        cancelImg.style.height = "2vh"
+  
+        cancelImg.classList.add("cancel-img")
+        divContainer.classList.add("div-container")
+
+        inputValue.value = ""
+    }
+})
